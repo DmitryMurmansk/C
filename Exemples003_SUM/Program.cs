81d1a8616d40e41eb2ed8d94a6fa88be770164ec
invalid51 <invalid@gmail.com>
2022-08-04 21:09:08 +0300

    C добавлением строк

--- Exemples003_SUM/Program.cs
+++ Exemples003_SUM/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 3;
-int numberb = 5;
-int result = numberA + numberb;
+﻿int numberA = new Random() .Next(1,10); // 1 2 3 4 ... 10
+Console.WriteLine(numberA);
+int numberB = new Random() .Next(1, 10);
+Console.WriteLine(numberB);
+int result = numberA + numberB;
 Console.WriteLine(result);
