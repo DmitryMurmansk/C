3582f1ce5ab3e6dce6de5840f82905256b33c373
invalid51 <invalid@gmail.com>
2022-08-04 21:55:43 +0300

    IfElse primer

---
+++ Exemples005_Condition_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользовтеля: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "лиля") // вот ТУТ после скобок не надо ставить знак ";"!!!!
+{
+    Console.WriteLine("Лиля, сделаешь красиво?");
+}
+else
+{
+    Console.Write("Приветствую тебя испытатель!");
+    Console.WriteLine(username);
+}
